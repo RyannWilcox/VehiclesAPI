e14cb69243d4f5e81d3f13693f7283387b74184b
Ryan Wilcox <rwilcox11@icloud.com>
2020-09-23 16:50:30 -0400

    Updated the pricing microservice to register with the eureka server Added the H2 database and converted the Price into an actual database entity.

--- pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
+++ pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
@@ -1,8 +1,7 @@
 package com.udacity.pricing.domain.price;
 
-import org.springframework.stereotype.Repository;
+import com.udacity.pricing.entity.Price;
+import org.springframework.data.repository.CrudRepository;
 
-@Repository
-public class PriceRepository {
-
+public interface PriceRepository extends CrudRepository<Price, Long> {
 }
