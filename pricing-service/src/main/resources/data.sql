dd9a053f3b9743150a17b1114a672e2bf61d2c3a
Ryan Wilcox <rwilcox11@icloud.com>
2020-09-25 16:13:45 -0400

    Updated the pricing service properties file added h2 database info.  Then created a data.sql file to add some items to the database

---
+++ pricing-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO price (vehicle_id,currency,price) VALUES (1,'USD',50000.00);
+INSERT INTO price (vehicle_id,currency,price) VALUES (2,'USD',60000.00);
+INSERT INTO price (vehicle_id,currency,price) VALUES (3,'USD',10000.00);
