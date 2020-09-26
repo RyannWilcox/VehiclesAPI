9ad03ad791d967728b3a3ad2375d8c4567254339
Ryan Wilcox <rwilcox11@icloud.com>
2020-09-26 14:26:55 -0400

    Added test case for pricing service

--- pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -2,15 +2,32 @@ package com.udacity.pricing;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class PricingServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@Autowired
+	private MockMvc mockMvc;
 
+	@Test
+	public void getPriceById() throws Exception {
+		mockMvc.perform(get("/prices/1"))
+						.andExpect(status().isOk())
+						.andExpect(content().contentType(MediaTypes.HAL_JSON_VALUE))
+						.andExpect(jsonPath("currency").value("USD"))
+						.andExpect(jsonPath("price").value(50000.00));
+	}
 }
